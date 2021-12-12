cfed207bb7d7748bcc739ee1dc7af148330fa7d1
anton.peraviortau <anton.peraviortau@jobilla.com>
2021-12-12 16:16:25 +0100

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post
+{
+    public  $title;
+    public  $excerpt;
+    public  $date;
+    public  $body;
+    public  $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title   = $title;
+        $this->excerpt = $excerpt;
+        $this->date    = $date;
+        $this->body    = $body;
+        $this->slug    = preg_replace('(\s)','-',mb_strtolower($title));
+    }
+
+    public static function all()
+    {
+        return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug,
+            ))
+            ->sortByDesc('date');
+    }
+
+    public static function find($slug)
+    {
+        return static::all()->firstWhere('slug', $slug);
+    }
+}
