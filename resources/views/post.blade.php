b05b13f988b3b2b0a77002693ced4a04cd1d6999
anton.peraviortau <anton.peraviortau@jobilla.com>
2022-02-04 12:44:44 +0100

    episode 24 finisheD

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,6 +6,10 @@
             {!! $post->title !!}
         </h1>
 
+        <p>
+            <a href="#">{{ $post->category->name }}</a>
+        </p>
+
         <div>
             {!! $post->body !!}
         </div>
